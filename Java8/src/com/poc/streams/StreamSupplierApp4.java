aba1ff2bfe80139b0b6aa54c2ed28ba12edd84ad
Rachit Shah <rachitinjava@gmail.com>
2017-12-17 16:17:14 +0530

    Java 8 Stream

--- Java8/src/com/poc/streams/StreamSupplierApp4.java
+++ Java8/src/com/poc/streams/StreamSupplierApp4.java
@@ -1,11 +1,26 @@
 package com.poc.streams;
 
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 /*Java 8 streams cannot be reused. As soon as you call any terminal operation the stream is closed*/
 public class StreamSupplierApp4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*Stream stream = Stream.of("a1","b1","c1").filter(s-> s.startsWith("a"));
+		
+		stream.anyMatch(s -> true);
+		
+		stream.noneMatch(s -> true);
+		*/
+		/*How to solve this?*/
+		
+		Supplier<Stream<String>> streamSupplier = () -> Stream.of("a1","b1","c1").filter(s-> s.startsWith("a"));
+		
+		streamSupplier.get().anyMatch(s -> true);
+		streamSupplier.get().noneMatch(s -> true);
+		
 	}
 
 }
