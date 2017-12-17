9e6a1e416dc8d6f5065daeb174f878737057edaa
Rachit Shah <rachitinjava@gmail.com>
2017-12-17 15:26:24 +0530

    Java 8 Stream

--- Java8/src/com/poc/streams/ProcessingOrderStreamApp4.java
+++ Java8/src/com/poc/streams/ProcessingOrderStreamApp4.java
@@ -6,11 +6,22 @@ public class ProcessingOrderStreamApp4 {
 
 	public static void main(String[] args) {
 
+		/*Example 1*/
 		Stream.of("a", "b", "c").filter(s -> {
 			System.out.println(s);
 			return true;
 		}).forEach(System.out::println);
 
+		/*Example 2*/
+		
+		Stream.of("a","b","c").map(s-> {
+			System.out.println("Mapping --> "+s);
+			return s.toUpperCase();
+		}).anyMatch(p-> {
+			System.out.println("Matching --> "+p);
+			return p.equals("B");
+		});
+		
 	}
 
 }
